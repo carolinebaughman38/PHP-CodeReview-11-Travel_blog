d92b9a52f036a9e4a17c07f2681e26a8469f352e
Marina Tukalo <marina-tukalo@hotmail.com>
2019-11-24 14:53:01 +0100

    add img and modify update.php

--- update.php
+++ update.php
@@ -1,36 +1,38 @@
 <?php 
-
-	require_once "dbconnect.php";
+ob_start();
+session_start();
+require_once 'dbconnect.php';
 
 	if(isset($_GET["id"])){
 		$concert_id = $_GET["id"];
-		$sql = "SELECT * FROM `concert` WHERE `id` = $concert_id";
-		$result = mysqli_query($conn ,$sql);
+		$sql_concert = "SELECT * FROM location INNER JOIN concert ON location.fk_concert_id=concert.id WHERE concert.id=$concert_id";
+		$result = mysqli_query($conn ,$sql_concert);
 		$row = $result->fetch_assoc();
 	}
 
 	if(isset($_POST["submit"])){
-	if((!empty($_FILES["uploaded_file"])) && ($_FILES['uploaded_file']['error'] == 0)) {
+	if((!empty($_FILES['uploaded_file'])) && ($_FILES['uploaded_file']['error'] == 0)) {
  	 $filename = basename($_FILES['uploaded_file']['name']);
   	$ext = substr($filename, strrpos($filename, '.') + 1);
+
   if (($ext == "jpg") && ($_FILES["uploaded_file"]["type"] == "image/jpeg") && 
-	($_FILES["uploaded_file"]["size"] < 800000000)) {
+	($_FILES["uploaded_file"]["size"] < 800000000000)) {
       $newname = dirname(_FILE_).'/uploads/'.$filename;
 // "uploads" is a folder inside of the main folder
       if (!file_exists($newname)) {
         if ((move_uploaded_file($_FILES['uploaded_file']['tmp_name'],$newname))) {
+        $concert_id = $_GET['id'];
         $concert_name = $_POST['concert_name'];
-  		$concert_short_description = $_POST['concert_short_description'];
-        $concert_web_address = $_POST['concert_web_address'];
         $date = $_POST['date'];
         $time = $_POST['time'];
+        $concert_short_description = $_POST['concert_short_description'];
         $ticket = $_POST['ticket'];
-  		$concert_id = $_GET['id'];
+        $concert_web_address = $_POST['concert_web_address'];
 
-	$sql = "UPDATE `concert` SET `concert_name`='$concert_name',`concert_short_description`='$concert_short_description',`concert_web_address`='$concert_web_address',`date`='$date',`time`='$time',`ticket`='$ticket',`concert_image`='$newname' WHERE `id`=$concert_id";
+  $sql_concert = "UPDATE concert SET concert_name='$concert_name',`date`='$date',`time`='$time',concert_short_description='$concert_short_description',ticket='$ticket',concert_web_address='$concert_web_address',concert_image='$newname' WHERE id=$concert_id";
 
-	if(mysqli_query($conn, $sql)){
-		//echo "<h3>Updated successfully</h3>";
+	if(mysqli_query($conn, $sql_concert)){
+		echo "<h3>Updated successfully</h3>";
 		header("Refresh:3; url=admin.php");
 	}
         } else {
@@ -43,19 +45,19 @@
      echo "Error: Only .jpg images under 350Kb are accepted for upload";
   }
 } else {
+        $concert_id = $_GET['id'];
         $concert_name = $_POST['concert_name'];
-        $concert_short_description = $_POST['concert_short_description'];
-        $concert_web_address = $_POST['concert_web_address'];
         $date = $_POST['date'];
         $time = $_POST['time'];
+        $concert_short_description = $_POST['concert_short_description'];
         $ticket = $_POST['ticket'];
-        $concert_id = $_GET["id"];
+        $concert_web_address = $_POST['concert_web_address'];
 
-	$sql = "UPDATE `concert` SET `concert_name`='$concert_name', `concert_short_description`='$concert_short_description',`concert_web_address`='$concert_web_address',`date`='$date',`time`='$time',`ticket`='$ticket' WHERE `id` = $concert_id";;
+	$sql_concert = "UPDATE concert SET concert_name='$concert_name',`date`='$date',`time`='$time',concert_short_description='$concert_short_description',ticket='$ticket',concert_web_address='$concert_web_address' WHERE `id`=$concert_id";
 
-	if(mysqli_query($conn, $sql)){
-		//echo "<h3>Updated successfully</h3>";
-		header("Refresh:2; url=admin.php");
+	if(mysqli_query($conn, $sql_concert)){
+		echo "<h3>Updated successfully without image!!</h3>";
+		header("Refresh:3; url=admin.php");
 	}
 }
 
@@ -82,8 +84,8 @@ $result = $res->fetch_all(MYSQLI_ASSOC);
   </head>
 
 <body>
-  <header>
-	<!-- navbar --->
+  <!--<header>-->
+	<!-- navbar 
    	<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
    		<a class="navbar-brand pl-5 my-3" href="index.php">Travel-o-matic blog</a>
    		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -106,25 +108,25 @@ $result = $res->fetch_all(MYSQLI_ASSOC);
    			</ul>
    		</div> 		
    	</nav>
-     </header>
+     </header> -->
    	<!-- form for update -->
-   	<div class="container mt-5">
+  <div class="container mt-5">
 		
 	<!-- form for update image -->
 	<form enctype="multipart/form-data" method="post">
 		<input type="hidden" name="id" value="<?php echo $row['id'] ?>">
 		<p>Concert name: <input type="text" name="concert_name" value="<?php echo $row['concert_name'] ?>"></p>
 		<p>Short description: <input type="text" name="concert_short_description" id="textarea" value="<?php echo $row['concert_short_description'] ?>"></p>
-    	<p>Date: <input type="date" name="date" value="<?php echo $row['date'] ?>"></p>
-        <p>Time: <input type="time" name="time" value="<?php echo $row['time'] ?>"></p>
-        <p>Price: <input type="text" name="ticket" value="<?php echo $row['ticket'] ?>"></p>
-        <p>Web Address: <input type="text" name="concert_web_address" value="<?php echo $row['concert_web_address'] ?>"></p>
+    <p>Date: <input type="date" name="date" value="<?php echo $row['date'] ?>"></p>
+    <p>Time: <input type="time" name="time" value="<?php echo $row['time'] ?>"></p>
+    <p>Price: <input type="text" name="ticket" value="<?php echo $row['ticket'] ?>"></p>
+    <p>Web Address: <input type="text" name="concert_web_address" value="<?php echo $row['concert_web_address'] ?>"></p>
 
-    	<input type="hidden" name="MAX_FILE_SIZE" value="1000000" /> 
-    	<p>Image: <img width="250px" src="<?php echo $row['concert_image'] ?>" alt=""></p>
-    	<p>Choose a file to upload: <input name="uploaded_file" type="file" />
-    	<input type="submit" value="Update" name="submit" /></p>
-   	</form> 
+    <input type="hidden" name="MAX_FILE_SIZE" value="1000000" /> 
+    <p>Image: <img width="250px" src="<?php echo $row['concert_image'] ?>" alt=""></p>
+    <p>Choose a file to upload: <input name="uploaded_file" type="file" />
+    <input type="submit" value="Update" name="submit" /></p>
+  </form> 
 
 	</div>
 
